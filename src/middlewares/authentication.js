9fbf97c54807addbcff747ce007c960bcbc49bfc
smyye <smyyeoztrk43@gmail.com>
2024-04-30 15:34:05 +0300

    authentication

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,18 +1,25 @@
 "use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+// app.use(authentication)
+
 const Token = require("../models/token");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
   const auth = req.headers?.authorization || null; // Token ...tokenKey...
   const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
 
-  if (tokenKey[0] == "Token") {
-    // SimpleToken:
+  if (tokenKey) {
+    if (tokenKey[0] == "Token") {
+      // SimpleToken:
 
-    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
-      "userId"
-    );
-    req.user = tokenData ? tokenData.userId : undefined;
+      const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+        "userId"
+      );
+      req.user = tokenData ? tokenData.userId : undefined;
+    }
   }
-
   next();
 };
